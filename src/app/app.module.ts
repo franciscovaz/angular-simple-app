9a705f57120716a1da937b0730d9ca7e401e4fdc
Francisco Vaz <ftomasvaz@gmail.com>
2020-10-18 14:34:27 +0100

    adding angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,28 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+import {
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatProgressSpinnerModule,
+} from "@angular/material";
 @NgModule({
   declarations: [AppComponent, HomeComponent, AboutComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
